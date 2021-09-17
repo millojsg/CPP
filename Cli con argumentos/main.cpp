88bcbe2c273fc21cc18c4363e06ce8c6f97d22b0
millojsg <millojsgprixus@gmail.com>
2021-09-17 20:03:37 -0300

    Se agrega ejemplo de aplicación cliente con pase de argumentos

---
+++ Cli con argumentos/main.cpp
@@ -0,0 +1,27 @@
+// Directiva entrada y salida.
+#include <iostream>
+
+// Espacio de nombres.
+using namespace std;
+
+// Punto de entrada.
+int main (int argc,char* argv[])
+{
+     // Si no se ingresan argumentos entonces:
+     if (argc == 1)
+     {
+          cout<<"El usuario no agregó argumentos de entrada. Intenta ejecutar el programa de la siguiente manera"<<endl;
+          cout<<"main.exe Argumento1 Argumento2 Argumento3 \"Argumento 4\"";
+     }
+     else // En caso de ingresar argumentos, mostrar en consola los argumentos.
+     {
+          for(int i=1 ; i<argc ; i++)
+          {
+               cout<<argv[i]<<endl;
+          }
+     }
+
+     cin.get();
+
+     return 0;
+}
